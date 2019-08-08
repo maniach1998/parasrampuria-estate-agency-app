7115170368af6a8e3d6823bdde5240843fa67b1a
Manas Acharekar <manas.acharekar98@gmail.com>
2019-08-09 00:24:36 +0530

    Added spinner on loading screen

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/LoginPage.css';
+import Spinner from '../images/spinner.svg';
 
 const LoadingScreen = () => (
     <div id="LoadingScreen" className="loading-container d-flex justify-content-center align-items-center">
-        <div className=" loading-container">
+        <div className="loading-container">
             <span className="company-name">Loading</span>
+            <img src={Spinner} alt="Spinner" style={{ width: 200 }} />
         </div>
     </div>
 );
